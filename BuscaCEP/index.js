5c3fcaf68a3f527ecf3a8fc0a9a979aa1de35530
Vitor <vitor.turqueti@aida.digital>
2023-09-29 22:36:36 -0300

    link para pagina que solicita CEP do usuário

--- BuscaCEP/index.js
+++ BuscaCEP/index.js
@@ -1,14 +1,3 @@
-// var cep = prompt("Forneça o CEP");
-// var req = new XMLHttpRequest();
-// req.onloadend = function(){
-//     resp = req.responseText;
-//     resp_obj = JSON.parse(resp);
-//     console.log(resp_obj)
-//     alert(`O CEP ${resp_obj.cep} pertence à rua ${resp_obj.logradouro} do bairro ${resp_obj.bairro}, ${resp_obj.localidade}/${resp_obj.uf}`);
-// }
-// req.open("GET", `https://viacep.com.br/ws/${cep}/json/`);
-// req.send(null);
-
 const tableWrapper = document.querySelector("#tableWrapper")
 
 function buscaCep() {
